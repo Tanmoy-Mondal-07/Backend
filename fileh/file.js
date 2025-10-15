956fd446e35db7f0656949dadbf5f90607cb7d45
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-10-15 20:00:44 +0530

    ..

---
+++ fileh/file.js
@@ -0,0 +1,17 @@
+const fs = require("fs")
+
+// fs.writeFileSync('./test.txt', 'hay there')
+
+// fs.writeFile('./test.txt', 'hay there',(err)=>{})
+
+// console.log(fs.readFileSync('./test.txt',"utf-8"))
+
+// fs.readFile('./test.txt', "utf-8", (error, result) => {
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log(result);
+//     }
+// })
+
+// fs.appendFileSync('./test.txt', new Date().toLocaleString()) // can use /n for next line
