c30b1bd440691dde3174be3462ddfd642bb1cdc7
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-10-17 19:53:42 +0530

    ..

--- httpServer/index.js
+++ httpServer/index.js
@@ -1,13 +1,21 @@
 const http = require("http")
 const fs = require('fs')
+const url = require('url')
 
 const myServer = http.createServer((req, res) => {
+
     const log = `${Date.now()}:${req.url} New Req Recived\n`
+
+    const myUrl = url.parse(req.url, true)
+    console.log(myUrl);
+    console.log(myUrl.pathname);
+
     fs.appendFile('log.txt', log, (err, data) => {
         res.end("hello from server")
     })
     // console.log(req);
     console.log('new req resived');
+
 });
 
 myServer.listen(8000, () => console.log('server started'))
