75b594e30b9325cb4f87e258a3f5f6ac79b81134
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-11-02 21:36:32 +0530

    ..

--- socketio/index.js
+++ socketio/index.js
@@ -1,7 +1,21 @@
-import express from 'express'
-import http from 'http'
+const express = require('express')
+const http = require('http')
+const path = require('path')
+const Server = require('socket.io')
 
 const app = express()
-
 const server = http.createServer(app)
+const io = new Server(server)
 
+//socket io
+io.on('connection', (socket) => {
+    console.log("a new user has connected", socket.id);
+})
+
+app.use(express.static(path.resolve('./public')))
+
+app.get('/', (req, res) => {
+    res.sendFile('/public/index.html')
+})
+
+server.listen(9000, () => console.log('Server running on http://localhost:9000'))
